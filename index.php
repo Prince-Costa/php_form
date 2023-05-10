88edebd0b7d89468f02b3b99b99814d4db1861c8
saroaremon <saroar01834@gmail.com>
2023-05-10 09:41:33 +0600

    email to number

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
 
 class FormData{  //initialing a class
-    public $email;  //intialing property of class
-    public $password;  //intialing property of class
+    public $num1;  //intialing property of class
+    public $num2;  //intialing property of class
 
-    public function setEmailPassword($email, $password) //intialing a method for set property value
+    public function setNumber($num1, $num2) //intialing a method for set property value
     {
-        $this->email = $email;
-        $this->password = $password;
+        $this->num1 = $num1;
+        $this->num2 = $num2;
     } 
     
     public function getData()  //intialing a method for get property value as a string
     {
-        $data = 'You Submitted <br>Email: '.$this->email.'<br> Password: '.$this->password;
+        // $data = 'You Sended <br>Number 1: '.$this->num1.'<br> Number 2: '.$this->num2.'<br> And, Ans is: '.$this->num1 +  $this->num2;
+        $data = $this->num1 +  $this->num2;
         return $data;
     }
 }
@@ -28,22 +29,26 @@ $formData1 = new FormData; //creating new object ot FormData class
 	<title>Post Request</title>
 </head>
 <body>
-    <div>
-        <?php
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {  //checking post method
-            $email = $_POST['email'];                //setting email data from $_POST
-             $password = $_POST['password'];         //setting password data from $_POST
-            $formData1->setEmailPassword($email, $password);  //calling setter method by new object
-            echo $formData1->getData().'<br><br><br>';        //calling getter method by new object
-        }        
-        ?>
-    </div>
+    <!-- <div>
+        
+    </div> -->
 	<form method="POST" action="">
-		<label for="email">Email:</label><br>
-		<input type="email" id="email" name="email"><br>
-		<label for="password">Password:</label><br>
-		<input type="password" id="password" name="password"><br>
-		<input type="submit" value="Submit">
+		<label for="num1">Number 1:</label><br>
+		<input type="text" id="num1" name="num1"><br>
+		<label for="num2">Number 2:</label><br>
+		<input type="text" id="num2" name="num2"><br>
+
+        <label for="result">Result</label><br>
+		<input type="text" id="result" value= <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {  //checking post method
+            $num1 = $_POST['num1'];                //setting num1 data from $_POST
+            $num2 = $_POST['num2'];         //setting num2 data from $_POST
+            $formData1->setNumber($num1, $num2);  //calling setter method by new object
+            echo "". $formData1->getData()."";        //calling getter method by new object
+        }        
+        ?>> <br>
+
+		<button type="submit" value="Sum">Sum</button>
 	</form>
 </body>
 </html>
